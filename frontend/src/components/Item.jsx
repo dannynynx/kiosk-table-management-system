ba8622e14408ad7ee9efde2ae1c5b34e1e201293
WiNg4205 <105526030+WiNg4205@users.noreply.github.com>
2024-03-08 16:28:38 +1100

    basic grid layout and item container layout

--- frontend/src/components/Item.jsx
+++ frontend/src/components/Item.jsx
@@ -3,9 +3,13 @@ import placeholderItem from '../assets/item-placeholder.svg';
 
 const Item = () => {
     return (
-        <>
+        <div className='item'>
             <img src={placeholderItem} className='item-image' alt="Placeholder Item"/>
-        </>
+            <div className='item-contents'>
+                <div className='item-name'>Title</div>
+                <div className='item-price'>$99.99</div>
+            </div>
+        </div>
     );
 };
 
