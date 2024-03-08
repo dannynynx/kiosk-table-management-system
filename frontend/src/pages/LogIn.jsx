b7cdf245e5110ff50b181e82f639998535b1c11f
Danny <z5358927@unsw.edu.au>
2024-03-09 00:47:37 +1100

    worked on login page, form is partially done and the design is done

--- frontend/src/pages/LogIn.jsx
+++ frontend/src/pages/LogIn.jsx
@@ -1,7 +1,21 @@
+import './LogIn.css';
+
 const LogIn = () => {
     return (
         <>
-            Login Page
+            <body>
+                <div className='container'>
+                    <form className='login-form'> 
+                        <h2 className='login-main-text'><b>Blue Zebra</b></h2>
+                        <p className='login-main-text'>Staff Login</p>
+                        <p className='input-label'>USERNAME</p>
+                        <input type='text' className='login-text-inputs' id='username'></input>
+                        <p className='input-label'>PASSWORD</p>
+                        <input type='text' className='login-text-inputs' id='password'></input>
+                        <input type='button' className='login-button' value='Login' id='login-submit'></input>
+                    </form>
+                </div>
+            </body>
         </>
     );
 };
