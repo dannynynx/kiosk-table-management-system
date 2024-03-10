2039bba9ebec2cc5026c00f96db5990395b4d15d
Danny <z5358927@unsw.edu.au>
2024-03-11 02:06:10 +1100

    started selection page

--- frontend/src/pages/KioskPage.jsx
+++ frontend/src/pages/KioskPage.jsx
@@ -1,15 +1,26 @@
 import './KioskPage.css';
 
 const KioskPage = () => {
+
+    const selectTableButton = () => {
+        document.getElementById('first-page').style.display = "none";
+        document.getElementById('selection-page').style.display = "flex";
+    };
+
     return (
         <>
             <body>
-                <div className='top-container'>
-                    <h1 className='kiosk-title'>Blue<br></br>Zebra</h1>
-                </div>
-                <div className='bottom-container'>
-                    <input type='button' className='kiosk-button' value='Select Table'></input>
-                </div>
+                <section className='first-page' id='first-page'>
+                    <div className='top-container'>
+                        <h1 className='kiosk-title'>Blue<br></br>Zebra</h1>
+                    </div>
+                    <div className='bottom-container'>
+                        <input type='button' className='kiosk-button' value='Select Table' onClick={selectTableButton}></input>
+                    </div>
+                </section>
+                <section className='selection-page' id='selection-page'>
+                    <h1 className='selection-title'>Select Table</h1>
+                </section>
             </body>
         </>
     );
