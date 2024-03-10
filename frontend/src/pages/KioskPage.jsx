c16ddd2b0cac4ac9801bfefbe508423a05602d3a
Danny <z5358927@unsw.edu.au>
2024-03-11 01:03:24 +1100

    created kiosk start page

--- frontend/src/pages/KioskPage.jsx
+++ frontend/src/pages/KioskPage.jsx
@@ -1,7 +1,16 @@
+import './KioskPage.css';
+
 const KioskPage = () => {
     return (
         <>
-            Kiosk Page
+            <body>
+                <div className='top-container'>
+                    <h1 className='kiosk-title'>Blue<br></br>Zebra</h1>
+                </div>
+                <div className='bottom-container'>
+                    <input type='button' className='kiosk-button' value='Select Table'></input>
+                </div>
+            </body>
         </>
     );
 };
