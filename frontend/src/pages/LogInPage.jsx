37fa121cf30ff3af44767f64cbe78ff941c27090
WiNg4205 <105526030+WiNg4205@users.noreply.github.com>
2024-03-11 12:19:54 +1100

    fixed vh bug for login page

--- frontend/src/pages/LogInPage.jsx
+++ frontend/src/pages/LogInPage.jsx
@@ -3,19 +3,17 @@ import './LogInPage.css';
 const LogInPage = () => {
     return (
         <>
-            <body>
-                <div className='container'>
-                    <form className='login-form'> 
-                        <h2 className='login-main-text'><b>Blue Zebra</b></h2>
-                        <p className='login-main-text'>Staff Login</p>
-                        <p className='input-label'>USERNAME</p>
-                        <input type='text' className='login-text-inputs' id='username'></input>
-                        <p className='input-label'>PASSWORD</p>
-                        <input type='password' className='login-text-inputs' id='password'></input>
-                        <input type='button' className='login-button' value='Login' id='login-submit'></input>
-                    </form>
-                </div>
-            </body>
+            <div className='container'>
+                <form className='login-form'>
+                    <h2 className='login-main-text'><b>Blue Zebra</b></h2>
+                    <p className='login-main-text'>Staff Login</p>
+                    <p className='input-label'>USERNAME</p>
+                    <input type='text' className='login-text-inputs' id='username'></input>
+                    <p className='input-label'>PASSWORD</p>
+                    <input type='password' className='login-text-inputs' id='password'></input>
+                    <input type='button' className='login-button' value='Login' id='login-submit'></input>
+                </form>
+            </div>
         </>
     );
 };
