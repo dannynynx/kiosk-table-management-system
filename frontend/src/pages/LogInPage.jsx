1cfc50439b58efabc063297ff9d3c6ab6203dcf6
WiNg4205 <105526030+WiNg4205@users.noreply.github.com>
2024-03-11 13:55:42 +1100

    working login functions

--- frontend/src/pages/LogInPage.jsx
+++ frontend/src/pages/LogInPage.jsx
@@ -1,6 +1,25 @@
+import { useState } from 'react';
+import axios from 'axios';
+import { useNavigate } from "react-router-dom"
 import './LogInPage.css';
 
 const LogInPage = () => {
+    const [formData, setFormData] = useState({});
+    const navigate = useNavigate();
+
+    const handleSubmit = async () => {
+        try {
+            await axios.post( 'http://127.0.0.1:5000/staff/staff_authentication', formData);
+            navigate('/menu')
+        } catch (error) {
+            console.error('Error submitting data:', error);
+        }
+    };
+
+    const handleChange = (e) => {
+        setFormData({ ...formData, [e.target.name]: e.target.value });
+    };
+
     return (
         <>
             <div className='container'>
@@ -8,10 +27,10 @@ const LogInPage = () => {
                     <h2 className='login-main-text'><b>Blue Zebra</b></h2>
                     <p className='login-main-text'>Staff Login</p>
                     <p className='input-label'>USERNAME</p>
-                    <input type='text' className='login-text-inputs' id='username'></input>
+                    <input type='text' className='login-text-inputs' id='username' name='username' onChange={handleChange} />
                     <p className='input-label'>PASSWORD</p>
-                    <input type='password' className='login-text-inputs' id='password'></input>
-                    <input type='button' className='login-button' value='Login' id='login-submit'></input>
+                    <input type='password' className='login-text-inputs' id='password' name='password' onChange={handleChange} />
+                    <input type='button' className='login-button' value='Login' id='login-submit' onClick={handleSubmit} />
                 </form>
             </div>
         </>
