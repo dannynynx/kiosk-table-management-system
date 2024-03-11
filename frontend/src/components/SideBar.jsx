ec30f307e5ee1718919a2fe46689c0b279f57945
WiNg4205 <105526030+WiNg4205@users.noreply.github.com>
2024-03-11 23:37:41 +1100

    basic layout

--- frontend/src/components/SideBar.jsx
+++ frontend/src/components/SideBar.jsx
@@ -1,7 +1,18 @@
+import './SideBar.css';
+import cart from '../assets/cart-icon.svg';
+import orderList from '../assets/list-icon.svg';
+
 const SideBar = () => {
+
     return (
         <>
-            Side Bar
+            <div className='sidebar-icon-container'>
+                <img src={cart} className='cart' alt='Cart Icon'/>
+            </div>
+            <div className='sidebar-icon-container'>
+                <img src={orderList} className='order-list' alt='Order List Icon'/>
+            </div>
+
         </>
     );
 };
