2527429a9914cb5a34ee1b5daa6bc1fceb576187
Ashley Wong <kurousa039@gmail.com>
2024-03-12 10:47:25 +1100

    laptop dyig

--- frontend/src/components/BottomBar.jsx
+++ frontend/src/components/BottomBar.jsx
@@ -1,12 +1,36 @@
 import "./BottomBar.css";
+import PopUp from "./PopUp";
+import { useState } from "react";
 
 const BottomBar = () => {
+    const [isPopUpVisible, setPopUpVisible] = useState(false);
+    const [popupMessage, setPopupMessage] = useState(""); // State to store the message
+
+    const handleAskForAssistance = () => {
+        setPopupMessage("Assistance is on the way");
+        setPopUpVisible(true);
+    };
+
+    const handleRequestBill = () => {
+        setPopupMessage("Please make your way to the counter");
+        setPopUpVisible(true);
+    };
+
+    const closePopUp = () => {
+        setPopUpVisible(false);
+    };
+
+
+
     return (
         <>
             <div className="bottom-bar">
-                <button className="bottom"><h3>Ask for assistance</h3></button>
-                <button className="bottom"><h3>Request the bill</h3></button>
+                <button className="bottom" onClick={handleAskForAssistance}><h3>Ask for assistance</h3></button>
+                <button className="bottom" onClick={handleRequestBill}><h3>Request the bill</h3></button>
             </div>
+        {isPopUpVisible && (
+            <PopUp message={popupMessage} onClose={closePopUp} />
+        )}
         </>
     );
 };
--- frontend/src/components/PopUp.jsx
+++ frontend/src/components/PopUp.jsx
@@ -1,8 +1,26 @@
-const PopUp = () => { 
+import "./PopUp.css";
+import zebra from "../assets/zebra.svg";
+
+const PopUp = (props) => { 
+
+    let popupStyle = {
+        display: "block",
+    };
+
+    const handleClose = () => { 
+        popupStyle = { 
+            display: "none",
+        }
+    }
+    
     return (<>
         
-        <div className="PopUp">
-            lalal
+        <div className="popup" style={popupStyle} onClick={handleClose}>
+            <div className="msg">
+                <img className="logo" src={zebra}></img>
+                <h4>{props.message}</h4>
+            </div>
+            
         </div>
 
     </>)
