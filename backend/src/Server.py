9f06fd5550bab33f21a77e1037a3d71d54066390
Yung Kim <z5361816@ad.unsw.edu.au>
2024-03-11 20:07:09 +1100

    modified server.py to contain correct imports and functions

--- backend/src/Server.py
+++ backend/src/Server.py
@@ -7,9 +7,11 @@ import os
 import sqlite3
 import shutil
 from InitDB import initialise_db
-from Customer import staff_tablet_authentication, confirm_table, authenticate_table, add_menu_item_to_cart, increase_menu_item_in_cart, decrease_menu_item_in_cart, show_table, select_table, go_back_table, show_menu, send_order_to_database
+from Customer import staff_tablet_authentication, confirm_table, authenticate_table, show_table, select_table, go_back_table, show_menu, send_order_to_database
+from flask_cors import CORS
 
 app = Flask(__name__)
+cors = CORS(app)
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 DB_PATH = os.path.join(BASE_DIR, 'BlueZebra.db')
