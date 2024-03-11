c7c48ebc38c61b6db06ad9184df520ccd859b696
Ashley wong <kurousa039@gmail.com>
2024-03-12 00:46:10 +1100

    did topbar - need to test

--- frontend/src/components/TopBar.jsx
+++ frontend/src/components/TopBar.jsx
@@ -1,7 +1,44 @@
+import zebra from "../assets/zebra.svg";
+import "./TopBar.css"
+import axios from 'axios';
+import React from "react";
+
 const TopBar = () => {
+
+
+    const categories = [];
+
+
+    React.useEffect(() => {
+        const getCategories = async () => {
+            try {
+                const response = await axios.get('http://127.0.0.1:5000/customer/categories');
+                for (const category of response.json()) { 
+                    categories.push({
+                        id: category.category_id,
+                        name: category.name,
+                    })
+                }
+            } catch (error) {
+                console.error('Error submitting data:', error);
+            }
+        }; 
+        getCategories();
+    });
+
+
     return (
         <>
-            Top Bar
+            <div className="topbar">
+                <img className="logo" src={zebra}></img>
+                <div className="categories">
+                {categories.map((category, key) => (
+                        <h2 className="category" id={category.id}>{category.name}</h2>
+                    
+                ))};
+                </div>
+            </div>
+           
         </>
     );
 };
