89053470e7969813b7cd8de2ec5da287a7056891
Danny <z5358927@unsw.edu.au>
2024-03-13 01:34:52 +1100

    fixed design

--- frontend/src/pages/KioskAuthenticationPage.jsx
+++ frontend/src/pages/KioskAuthenticationPage.jsx
@@ -14,7 +14,7 @@ const KioskAuthenticationPage = () => {
         <>
             <div className='container'>
                 <div className='box-container'>
-                    <p className='kiosk-title'>Blue Zebra</p>
+                    <p className='authentication-title'>Blue Zebra</p>
                     <p className='text-description'>Please Type the four-digit code here:</p>
                     <input type="text" className='code-input' />
                     <input type="button" className='code-confirmation' value='Confirm'  onClick={handleSubmit}/>
