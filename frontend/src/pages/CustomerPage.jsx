18720b767e085098ac98e467f5fb36d3b88823da
Ashley wong <kurousa039@gmail.com>
2024-03-23 12:55:01 +1100

    code for table need to test

--- frontend/src/pages/CustomerPage.jsx
+++ frontend/src/pages/CustomerPage.jsx
@@ -8,10 +8,10 @@ import Item from "../components/Item.jsx";
 import { useInitialiseMenu, useMenu } from "../context/MenuContext.jsx";
 import { useEffect } from "react";
 import axios from "axios";
-// import { useLocation } from 'react-router-dom';
+import { useLocation } from 'react-router-dom';
 
 const CustomerPage = (props) => {
-    // const location = useLocation();
+    const location = useLocation();
     const initialiseMenuItem = useInitialiseMenu();
     const getMenu = useMenu();
     useEffect(() => {
@@ -35,7 +35,7 @@ const CustomerPage = (props) => {
     return (
         <>
             <div className='topbar-container'>
-                <TopBar tablenumber={1}/>
+                <TopBar tablenumber={location.state.tablenumber}/>
             </div>
             <div className='sidebar-container'>
                 <SideBar/>
