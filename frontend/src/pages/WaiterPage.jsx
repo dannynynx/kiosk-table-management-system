d46e38e1cf548be32e2b0f1d457b111ad4d42fc3
Ashley Wong <kurousa039@gmail.com>
2024-04-01 15:27:39 +1100

    wait stagf

--- frontend/src/pages/WaiterPage.jsx
+++ frontend/src/pages/WaiterPage.jsx
@@ -1,11 +1,18 @@
 import './WaiterPage.css';
 import {useEffect, useState} from "react";
+import axios from "axios";
 
 const WaiterPage = () => {
     const [notifs, setNotifs] = useState([])
     
     useEffect(() => {
-        //get ordersS
+        const fetchData = async () => {
+            try {
+                const response = await axios.get('http://127.0.0.1:5000/kitchen/get_order_status')
+            } catch {
+
+            }
+        };
     }, []);
    
 
