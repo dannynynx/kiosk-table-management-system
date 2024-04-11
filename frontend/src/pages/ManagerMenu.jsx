afddac39b9a88d93f843341ab0cd7f94aa538be4
Ashley Wong <kurousa039@gmail.com>
2024-04-11 15:19:29 +1000

    added manager menu pages

---
+++ frontend/src/pages/ManagerMenu.jsx
@@ -0,0 +1,61 @@
+import SideBar from '../components/SideBar.jsx';
+import TopBar from '../components/TopBar.jsx';
+import './ManagerMenu.css';
+import PropTypes from "prop-types";
+import Menu from "../components/Menu.jsx";
+import Item from "../components/Item.jsx";
+import { useInitialiseMenu } from "../context/MenuContext.jsx";
+import { useEffect, useState } from "react";
+import axios from "axios";
+import { useNavigate } from 'react-router-dom';
+
+const ManagerMenu = (props) => {
+    const initialiseMenuItem = useInitialiseMenu();
+    const [selectedCategory, setSelectedCategory] = useState(null);
+    const navigate = useNavigate();
+    const [searchValue, setSearchValue] = useState('');
+
+    useEffect(() => {
+        if (!localStorage.getItem('token')) { 
+            navigate('/login');
+        } 
+        axios.get('http://127.0.0.1:5000/customer/showMenu')
+        .then(response => {
+            const data = response.data;
+            initialiseMenuItem(data);
+        })
+        .catch(error => {
+            console.error('Error fetching menu:', error);
+        });
+    }, [initialiseMenuItem]);
+
+    const handleCategoryFilter = (category) => {
+        setSelectedCategory(category);
+    };
+
+    const handleSearchFilter = (searchValue) => {
+        setSearchValue(searchValue);
+    }
+
+    const { display } = props;
+    return (
+        <>
+            <div className='topbar-container'>
+                <TopBar onHandleSearchFilter={handleSearchFilter}/>
+            </div>
+            <div className='sidebar-container'>
+                <SideBar onCategorySelect={handleCategoryFilter}/>
+            </div>
+            <div className='main-content-container'>
+                {display === 'menu' && <Menu selectedCategory={selectedCategory} searchValue={searchValue}/>}
+                {display === 'item' && <Item />}
+            </div>
+        </>
+    );
+};
+
+ManagerMenu.propTypes = {
+    display: PropTypes.string.isRequired,
+};
+
+export default ManagerMenu;
