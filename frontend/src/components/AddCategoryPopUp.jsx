0ac7d156e3fea69fc3c7b734620bc32afe08a26d
Ashley Wong <kurousa039@gmail.com>
2024-04-14 13:05:29 +1000

    basic add frontend working - need backend

--- frontend/src/components/AddCategoryPopUp.jsx
+++ frontend/src/components/AddCategoryPopUp.jsx
@@ -1,10 +1,17 @@
 import "./AddCategoryPopUp.css";
 import backArrow from '../assets/back-arrow-icon.svg';
 import PropTypes from "prop-types";
+import { useRef } from 'react';
 
 const AddCategoryPopUp = (props) => { 
 
     const popupClassName = "add-popup" + (props.isPopUpVisible ? " visible" : "");
+    const inputRef = useRef();
+
+    const handleAddCategory= (event) => { 
+        alert('A name was submitted: ' + inputRef.current.value);
+        event.preventDefault();
+    }
     
     return (
         <>
@@ -13,10 +20,10 @@ const AddCategoryPopUp = (props) => {
                 <button onClick={props.onClose} className="close-button">
                     <img src={backArrow} alt='Back Arrow Icon'></img>
                 </button>
-                <form className="add-category">
+                <form className="add-category" onSubmit={handleAddCategory}>
                     <h2>Add Category</h2>
                     <label>Category name</label>
-                    <input type='text' name="category-name" placeholder='category name'/>
+                    <input type='text' name="category-name" ref={inputRef} placeholder='category name'/>
                     <button type="submit">Add category</button>
                 </form>
             </div>
