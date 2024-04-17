bd90c7f81ce32941f7f0b627882a8a968a648785
Ashley Wong <kurousa039@gmail.com>
2024-04-17 10:25:36 +1000

    add menu item half fix

--- frontend/src/components/ItemAdd.jsx
+++ frontend/src/components/ItemAdd.jsx
@@ -75,6 +75,7 @@ const ItemAdd = () => {
             }
           });
           console.log(response.data);
+          navigate('/manager/menu')
         } catch (error) {
           console.error('Error uploading file:', error);
         }
